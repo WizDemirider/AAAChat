34b162e4e9daddd2366ca7aaa7cf8c17ffbf7104
Ankit Gupta <argankit@gmail.com>
2019-10-09 18:34:20 +0530

    incomplete frontend stuff

---
+++ resources/js/components/ChatApp.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="chat-app">
+        <Conversation :contact="selectedContact" :messages="messages"/>
+        <ContactsList :contacts="contacts" />
+    </div>
+</template>
+
+<script>
+    import Conversation from './Conversation';
+    import ContactsList from './ContactsList';
+    export default {
+        data() {
+            return {
+                selectedContact: null,
+                messages: [],
+                contacts: []
+            }
+        },
+        mounted() {
+            axios.get('/contacts').then((response) => {
+                console.log(response.data);
+                this.contacts = response.data;
+            })
+            console.log('Component mounted.');
+        },
+        components: {Conversation,ContactsList}
+    }
+</script>
---
+++ resources/js/components/ContactsList.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class = "contacts-list"></div>
+</template>
+<script>
+export default {
+
+}
+</script>
